184f48cca4cf1309051bf20747c4b88496257329
Rafael Ibraim <ibraim.gm@gmail.com>
2012-12-28 10:10:06 -0200

    Added Android support
    These are not the same thing as 'Java Support'. The 'android-mode' relies on a
    working setup of the android-sdk and his associated tools.

---
+++ custom/lang-android.el
@@ -0,0 +1,12 @@
+;;; -*- coding: utf-8-unix; -*-
+;;; Emacs-DevTools - An easy emacs setup for developers
+;;;
+;;; Copyright (C) 2012 by it's authors.
+;;; All rights reserved. See LICENSE, AUTHORS.
+;;;
+;;; lang-android.el --- Editing support for Android projects
+
+;; require
+(require 'android-mode)
+
+(provide 'lang-android)
