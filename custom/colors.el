db17387576d2788839a605202f0ffddcf1f10538
Rafael Ibraim <ibraim.gm@gmail.com>
2013-04-06 16:25:51 -0300

    Tweaked some extra color highlight for clojure

--- custom/colors.el
+++ custom/colors.el
@@ -14,15 +14,22 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/el-get/color-theme-solarized")
 (load-theme 'solarized-dark t)
 
-;; Some lisp-specific regexp colors.
+;; Some common lisp specific regexp colors.
 (font-lock-add-keywords 'lisp-mode
                        '(("(\\(\\(define-\\|def\\|do-\\|with-\\|set\\|get\\|make-\\|and\\|or\\|not\\|null\\)\\(\\s_\\|\\w\\)*\\)"
                           1 font-lock-keyword-face)))
 
-(font-lock-add-keywords 'lisp-mode
-                        '(("\\(\\+[a-zA-Z0-9\\-]+\\+\\)" 1 font-lock-constant-face)))
+(font-lock-add-keywords 'lisp-mode '(("\\(\\+[a-zA-Z0-9\\-]+\\+\\)" 1 font-lock-constant-face)))
 
-(font-lock-add-keywords 'lisp-mode
-                        '(("\\(\\*[a-zA-Z0-9\\-]+\\*\\)" 1 font-lock-variable-name-face)))
+(font-lock-add-keywords 'lisp-mode '(("\\(\\*[a-zA-Z0-9\\-]+\\*\\)" 1 font-lock-variable-name-face)))
+
+;; Extra colors for clojure
+(make-face 'clojure-global-var-face)
+(make-face 'clojure-keyword-face)
+(set-face-foreground 'clojure-global-var-face "salmon")
+(set-face-foreground 'clojure-keyword-face "light slate blue")
+
+(font-lock-add-keywords 'clojure-mode '(("\\(@?\\*[a-zA-Z0-9\\-\\?]+\\*\\)" 1 'clojure-global-var-face)))
+(font-lock-add-keywords 'clojure-mode '(("\\(\\^?:[a-zA-Z0-9\\?\\#\\-]+\\)" 1 'clojure-keyword-face)))
 
 (provide 'colors)
