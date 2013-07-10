20bafa7ea465365c646abcde288a123dcc23e892
Rafael Ibraim <ibraim.gm@gmail.com>
2013-07-10 17:46:15 -0300

    Changed C indent for case label

--- custom/lang-c.el
+++ custom/lang-c.el
@@ -6,11 +6,14 @@
 ;;;
 ;;; lang-c.el --- Editing options for C
 
+
+;;; see http://www.emacswiki.org/emacs/IndentingC for explanation
 (setq-default c-default-style "linux"
               c-basic-offset 2)
 
 (add-hook 'c-mode-hook
           (lambda ()
+            (c-set-offset 'case-label 2)
             (define-key c-mode-map (kbd "\r") 'newline-and-indent)))
 
 (add-hook 'c++-mode-hook
