2b8768fb0cbc4218865c6d9f7ca5eca4d4bccd1c
Rafael Ibraim <ibraim.gm@gmail.com>
2017-08-13 13:26:43 -0300

    Refactored 'completion' and 'lang' modules

--- lisp/completion-init.el
+++ lisp/completion-init.el
@@ -9,23 +9,36 @@
 
 ;;;###autoload
 (defun dt-completion-init ()
-  "Initialize the completion framework"
-  (global-set-key (kbd "C-c t") 'sr-speedbar-toggle)
-  (setq counsel-git-grep-cmd-default "git --no-pager grep --full-name -n --no-color -i -e \"%s\"")
-  (setq counsel-ag-base-command "ag --vimgrep --nocolor --nogroup %s")
-  (global-set-key "\C-s" 'swiper)
-  (global-set-key (kbd "C-c C-r") 'ivy-resume)
-  (global-set-key (kbd "M-x") 'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
-  (global-set-key (kbd "C-c g") 'counsel-git)
-  (global-set-key (kbd "C-c j") 'counsel-git-grep)
-  (global-set-key (kbd "C-c k") 'counsel-ag)
+  "Initialize the completion framework."
+
+  ;; timer dependent config
+  (dt-config-purpose/timer)
+  (dt-config-company/timer)
+
+  ;; idle timer dependent config
+  (dt-config-ivy/idle)
+
+  ;; immediate/autoload config
   (global-set-key (kbd "C-x g") 'magit-status)
-  (global-set-key (kbd "<f11>") 'purpose-toggle-window-purpose-dedicated)
-  (global-set-key (kbd "C-c l l") 'purpose-load-window-layout)
-  (global-set-key (kbd "C-c l s") 'purpose-save-window-layout)
-  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
-  ;; company config
+  (dt-config-dumb-jump)
+  (dt-config-projectile)
+  (dt-config-project-explorer)
+
+  ;; timer init
+  (run-with-timer
+   1 nil
+   (lambda ()
+     (require 'window-purpose)
+     (require 'company)))
+
+  ;; idle init
+  (run-with-idle-timer
+   1 nil
+   (lambda ()
+     (ivy-mode t)
+     (dt-config-cheatsheet))))
+
+(defun dt-config-company/timer ()
   (eval-after-load "company"
     '(progn
        (setq company-idle-delay 0.3)
@@ -51,8 +64,19 @@
        (set-face-attribute 'company-tooltip-selection nil :background "orange1" :foreground "black")
        (set-face-attribute 'company-tooltip-mouse nil :foreground "red" :background "green3")
        (set-face-attribute 'company-tooltip-search nil :foreground "red" :background "yellow")
-       (set-face-attribute 'company-tooltip-search-selection nil :foreground "red" :background "orange1")))
-  ;; ivy config
+       (set-face-attribute 'company-tooltip-search-selection nil :foreground "red" :background "orange1"))))
+
+(defun dt-config-ivy/idle ()
+  (setq counsel-git-grep-cmd-default "git --no-pager grep --full-name -n --no-color -i -e \"%s\"")
+  (setq counsel-ag-base-command "ag --vimgrep --nocolor --nogroup %s")
+  (global-set-key "\C-s" 'swiper)
+  (global-set-key (kbd "C-c C-r") 'ivy-resume)
+  (global-set-key (kbd "M-x") 'counsel-M-x)
+  (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+  (global-set-key (kbd "C-c g") 'counsel-git)
+  (global-set-key (kbd "C-c j") 'counsel-git-grep)
+  (global-set-key (kbd "C-c k") 'counsel-ag)
+  (define-key read-expression-map (kbd "C-r") 'counsel-expression-history)
   (eval-after-load "ivy"
     '(progn
        (setq ivy-use-virtual-buffers t)
@@ -63,44 +87,7 @@
 	       (ivy-switch-buffer . ivy-sort-function-buffer)
 	       (counsel-find-file . dt--ivy-sort-files-function)))
        (define-key ivy-minibuffer-map (kbd "C-j") #'ivy-immediate-done)
-       (define-key ivy-minibuffer-map (kbd "RET") #'ivy-alt-done)))
-
-  ;; dumb-jump
-  (bind-lazy-cmds (("C-M-p" . dumb-jump-back)
-		   ("C-M-g" . dumb-jump-go))
-		  (dumb-jump-mode))
-
-  ;;project commands
-  (bind-lazy-cmds (("C-c p m" . projectile-commander)
-		   ("C-c p t" . projectile-toggle-between-implementation-and-test)
-		   ("C-c p P" . projectile-test-project)
-		   ("C-c p c" . projectile-compile-project)
-		   ("C-c p i" . projectile-invalidate-cache)
-		   ("C-c p b" . counsel-projectile-switch-to-buffer)
-		   ("C-c p d" . counsel-projectile-find-dir)
-		   ("C-c p f" . counsel-projectile-find-file)
-		   ("C-c p p" . counsel-projectile-switch-project))
-		  (projectile-global-mode)
-		  (counsel-projectile-on))
-  ;; sr-speedbar
-  (eval-after-load "sr-speedbar"
-    '(progn
-       (setq sr-speedbar-right-side nil)
-       (setq sr-speedbar-skip-other-window-p t)))
-
-  ;; timer init
-  (run-with-timer
-   1 nil
-   (lambda ()
-     (require 'company)))
-  ;; idle init
-  (run-with-idle-timer
-   1 nil
-   (lambda ()
-     (ivy-mode t)
-     (require 'window-purpose)
-     (purpose-mode)
-     (dt--configure-cheatsheet))))
+       (define-key ivy-minibuffer-map (kbd "RET") #'ivy-alt-done))))
 
 (defun dt--ivy-sort-files-function (_name candidates)
   "Sort candidates by size, and then by lexicographic order."
@@ -117,18 +104,47 @@
   "Like ivy-rregex-plus, but always trating `.` as a literal character."
   (ivy--regex-plus (replace-regexp-in-string "\\." "\\." str nil t)))
 
-(defmacro bind-lazy-cmds (commands &rest activation-forms)
-  `(progn
-     ,@(mapcar (lambda (e)
-		 `(global-set-key (kbd ,(car e))
-				  (lambda ()
-				    (interactive)
-				    ,@(mapcar (lambda (e)  `(global-unset-key (kbd ,(car e)))) commands)
-				    ,@activation-forms
-				    (,(cdr e)))))
-	       commands)))
+(defun dt-config-purpose/timer ()
+  (global-set-key (kbd "<f11>") 'purpose-toggle-window-purpose-dedicated)
+  (global-set-key (kbd "C-c l l") 'purpose-load-window-layout)
+  (global-set-key (kbd "C-c l s") 'purpose-save-window-layout)
+  (eval-after-load "window-purpose"
+    '(progn
+       (purpose-mode))))
 
-(defun dt--configure-cheatsheet ()
+(defun dt-config-dumb-jump ()
+  (global-set-key (kbd "C-M-g") 'dumb-jump-go)
+  (global-set-key (kbd "C-M-p") 'dumb-jump-back)
+  (eval-after-load "dumb-jump"
+    '(progn
+       (dumb-jump-mode))))
+
+(defun dt-config-projectile ()
+  (global-set-key (kbd "C-c p m") 'projectile-commander)
+  (global-set-key (kbd "C-c p t") 'projectile-toggle-between-implementation-and-test)
+  (global-set-key (kbd "C-c p P") 'projectile-test-project)
+  (global-set-key (kbd "C-c p c") 'projectile-compile-project)
+  (global-set-key (kbd "C-c p i") 'projectile-invalidate-cache)
+  (global-set-key (kbd "C-c p b") 'counsel-projectile-switch-to-buffer)
+  (global-set-key (kbd "C-c p d") 'counsel-projectile-find-dir)
+  (global-set-key (kbd "C-c p f") 'counsel-projectile-find-file)
+  (global-set-key (kbd "C-c p p") 'counsel-projectile-switch-project)
+  (eval-after-load "projectile"
+    '(progn
+       (projectile-global-mode)
+       (counsel-projectile-on)))
+  (eval-after-load "counsel-projectile"
+    '(progn
+       (counsel-projectile-on))))
+
+(defun dt-config-project-explorer ()
+  (global-set-key (kbd "C-c t") 'sr-speedbar-toggle)
+  (eval-after-load "sr-speedbar"
+    '(progn
+       (setq sr-speedbar-right-side nil)
+       (setq sr-speedbar-skip-other-window-p t))))
+
+(defun dt-config-cheatsheet ()
   (cheatsheet-add :group 'Display :key "C-x C-+" :description "text-scale-adjust")
   (cheatsheet-add :group 'Navigation :key "C-c r" :description "iy-resume")
   (cheatsheet-add :group 'Navigation :key "C-c g" :description "counsel-git")
--- lisp/lang-init.el
+++ lisp/lang-init.el
@@ -9,41 +9,49 @@
 ;;;###autoload
 (defun dt-lang-init ()
   "Initialize language-specific configuration"
-  (lang-smartparens-init)
-  (lang-flycheck)
-  (lang-rainbow-delimiters)
-  (lang-purpose)
-  (lang-python-init)
-  (lang-cucumber-init)
-  (lang-web-init)
-  (lang-markdown-init))
 
-(defun lang-smartparens-init ()
+  ;; timer dependent config
+  (dt-lang-flycheck/timer)
+
+  ;; immediate/autoload config
+  (dt-lang-smartparens)
+  (dt-lang-rainbow-delimiters)
+  (dt-lang-purpose)
+  (dt-lang-python)
+  (dt-lang-cucumber)
+  (dt-lang-web)
+  (dt-lang-markdown)
+
+  (run-with-timer
+   1 nil
+   (lambda ()
+     (global-flycheck-mode))))
+
+(defun dt-lang-smartparens ()
   (require 'smartparens-config)
   (add-hook 'emacs-lisp-mode-hook 'smartparens-mode)
   (add-hook 'web-mode-hook 'smartparens-mode)
-  (sp-local-pair 'web-mode "{" nil :post-handlers '(:add curly-braces-on-enter) :when '(("RET"))))
+  (sp-local-pair 'web-mode "{" nil :post-handlers '(:add dt--curly-braces-on-enter) :when '(("RET"))))
 
-(defun curly-braces-on-enter (id action context)
+(defun dt--curly-braces-on-enter (id action context)
   (when (eq action 'insert)
     (newline)
     (indent-according-to-mode)
     (previous-line)
     (indent-according-to-mode)))
 
-(defun lang-flycheck ()
+(defun dt-lang-flycheck/timer ()
   (eval-after-load "flycheck"
     '(progn
        (flycheck-add-mode 'javascript-eslint 'web-mode)
        (when (memq system-type '(ms-dos windows-nt))
-	 (setq flycheck-xml-parser 'flycheck-parse-xml-region))))
-  (global-flycheck-mode))
+	 (setq flycheck-xml-parser 'flycheck-parse-xml-region)))))
 
-(defun lang-rainbow-delimiters ()
+(defun dt-lang-rainbow-delimiters ()
   (add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
   (add-hook 'web-mode-hook 'rainbow-delimiters-mode))
 
-(defun lang-purpose ()
+(defun dt-lang-purpose ()
   (eval-after-load "window-purpose"
     '(progn
        (add-to-list 'purpose-user-mode-purposes '(python-mode . py))
@@ -51,7 +59,7 @@
        (purpose-compile-user-configuration)
        (define-key purpose-mode-map (kbd "C-x b") 'ivy-switch-buffer))))
 
-(defun lang-python-init ()
+(defun dt-lang-python ()
   (global-set-key (kbd "C-x p") 'dt-pyvenv-workon)
   (eval-after-load "python"
     '(progn
@@ -75,12 +83,12 @@
   (call-interactively 'python-shell-send-region)
   (deactivate-mark)))
 
-(defun lang-cucumber-init ()
+(defun dt-lang-cucumber ()
   (setq feature-default-i18n-file (concat dt-data-dir "i18n.yml"))
   (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode)))
 
-(defun lang-web-init ()
-  (add-hook 'web-mode-hook 'dt-web-mode-hook)
+(defun dt-lang-web ()
+  (add-hook 'web-mode-hook 'dt--web-mode-hook)
   (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.json\\'" . web-mode))
   (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
@@ -89,7 +97,7 @@
        (setq web-mode-content-types-alist
 	     '(("jsx" . "\\.js[x]?\\'"))))))
 
-(defun dt-web-mode-hook ()
+(defun dt--web-mode-hook ()
   (setq web-mode-markup-indent-offset 2)
   (setq web-mode-css-indent-offset 2)
   (setq web-mode-code-indent-offset 2)
@@ -98,7 +106,7 @@
   (setq web-mode-enable-current-element-highlight t)
   (setq web-mode-enable-current-column-highlight t))
 
-(defun lang-markdown-init ()
+(defun dt-lang-markdown ()
   (eval-after-load "markdown-mode"
     '(progn
        (define-key markdown-mode-map (kbd "C-c C-c t") 'markdown-toc-generate-toc))))
